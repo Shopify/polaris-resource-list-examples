6d2b77945a32147db92cc8fe9f74528918131d67
Ryan Frederick <ryan@ryanfrederick.com>
2018-01-29 20:58:34 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Page,
+  Card,
+} from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Page title="Polaris resource list examples">
+        <Card title="Example 1" sectioned>
+          <p>Coming soon</p>
+        </Card>
+      </Page>
+    );
+  }
+}
+
+export default App;
